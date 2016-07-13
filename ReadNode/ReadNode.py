d8a2134ed11f90d70e1666c95ae228c4404ed6fe
Bubenshchikov <vpalex@mail.ru>
2016-07-13 21:29:12 +0300

    u

--- ReadNode/ReadNode.py
+++ ReadNode/ReadNode.py
@@ -31,7 +31,7 @@ class ReadNode(object):
      boardCS=[]
      boardMG=[]
      boardCCS=[]
-     boardLID=[]
+     boardLI=[]
               
      # массив с найденными релизами в xml
      releases = [] 
